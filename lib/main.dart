f1fa34342b9277fc170e3955d100a9af56ea274c
Emmanuel Hernandez <eherdezmoran@gmail.com>
2020-05-03 20:49:33 -0500

    fonts and stlye added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,34 +10,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          title: TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed'
+          ),
+        )
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeals'),
-      ),
-      body: Center(
-        child: Text('Navigation Time!')
-      ),
+      home: CategoriesScreen(),
     );
   }
 }
